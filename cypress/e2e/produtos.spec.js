04d9cf5161bc757a2564b3415367f761c97c9075
Kauã Ribeiro <kauaribeirodev@outlook.com>
2025-10-17 15:25:53 -0300

    teste automatizados em listas de produtos

---
+++ cypress/e2e/produtos.spec.js
@@ -0,0 +1,26 @@
+describe("Funcionalidade Página de produtos", () => {
+  beforeEach(() => {
+    cy.visit("http://lojaebac.ebaconline.art.br/produtos/");
+  });
+
+  it("Deve selecionar um produto da lista", () => {
+    cy.get(".product-block")
+      //.first()
+      //.last()
+      //.eq(3)
+      .contains("Arcadio Gym Short")
+      .click();
+  });
+
+  it.only("Deve selecionar um produto da lista", () => {
+    let quantidade = 6;
+
+    cy.get(".product-block").contains("Arcadio Gym Short").click();
+    cy.get(".button-variable-item-34").click();
+    cy.get(".button-variable-item-Black").click();
+    cy.get(".input-text").clear().type(quantidade);
+    cy.get(".single_add_to_cart_button").click();
+
+    cy.get("#cart").should("contain", quantidade);
+  });
+});
