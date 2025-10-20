18ea606197534f4ea6c7b2b883597a1bc1eb39fa
Kauã Ribeiro <kauaribeirodev@outlook.com>
2025-10-20 19:01:52 -0300

    archer

--- cypress/e2e/login.spec.js
+++ cypress/e2e/login.spec.js
@@ -1,30 +1,32 @@
-describe('Funcionalidade Login', () => {
-
+describe("Funcionalidade Login", () => {
   beforeEach(() => {
-    cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+    cy.visit("minha-conta/");
   });
 
-  it('passes', () => {
-    cy.get('[name="username"]').type('teste@teste123.com');
-    cy.get('#password').type('teste@teste123.');
+  it("passes", () => {
+    cy.get('[name="username"]').type("teste@teste123.com");
+    cy.get("#password").type("teste@teste123.");
     cy.get('#customer_login [name="login"]').click();
 
-    cy.get('.page-title').should('contain', 'Minha conta')
-  })
+    cy.get(".page-title").should("contain", "Minha conta");
+  });
 
-  it('Deve exibir uma mensagem de erro ao inserir senha inválida', () => {
-    cy.get('[name="username"]').type('teste@teste123.com');
-    cy.get('#password').type('teste@teste123');
-    cy.get('#customer_login [name="login"]').click();
-    
-    cy.get('.woocommerce-error').should('contain', 'Erro: A senha')
-  })
-
-    it('Deve exibir uma mensagem de erro ao inserir email inválido', () => {
-    cy.get('[name="username"]').type('teste@teste123.comm');
-    cy.get('#password').type('teste@teste123.');
+  it("Deve exibir uma mensagem de erro ao inserir senha inválida", () => {
+    cy.get('[name="username"]').type("teste@teste123.com");
+    cy.get("#password").type("teste@teste123");
     cy.get('#customer_login [name="login"]').click();
 
-    cy.get('.woocommerce-error').should('contain', 'Endereço de e-mail desconhecido')
-  })
-})
+    cy.get(".woocommerce-error").should("contain", "Erro: A senha");
+  });
+
+  it("Deve exibir uma mensagem de erro ao inserir email inválido", () => {
+    cy.get('[name="username"]').type("teste@teste123.comm");
+    cy.get("#password").type("teste@teste123.");
+    cy.get('#customer_login [name="login"]').click();
+
+    cy.get(".woocommerce-error").should(
+      "contain",
+      "Endereço de e-mail desconhecido"
+    );
+  });
+});
