cd6c7ac4e4c3d1fa41feca73a781d74cffb97732
Kauã Ribeiro <kauaribeirodev@outlook.com>
2025-10-27 20:18:07 -0300

    adição-new-itens

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    projectId: "2w2kvz",
     baseUrl: "http://lojaebac.ebaconline.art.br/",
     specPattern: "cypress/e2e/**/*.spec.js", // ou .cy.js, dependendo da extensão que você usa
     setupNodeEvents(on, config) {
